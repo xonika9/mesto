47e3f4bb8995fcde83a72e7ccc6aa9c542028a5c
Alex Beltyukov <xonika9@gmail.com>
2022-03-31 21:44:02 +0800

    исправил ошибки

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,26 @@
-const openButton = document.querySelector(".profile__edit-button");
+const popupOpenButton = document.querySelector(".profile__edit-button");
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__about");
-
 const modalWindow = document.querySelector(".popup");
 const editForm = modalWindow.querySelector(".popup__form");
 const nameInput = modalWindow.querySelector(".popup__input_type_name");
 const aboutInput = modalWindow.querySelector(".popup__input_type_about");
 const submitButton = modalWindow.querySelector(".popup__submit-button");
-const closeButton = modalWindow.querySelector(".popup__close-button");
-
+const popupCloseButton = modalWindow.querySelector(".popup__close-button");
 function toggleModalWindow() {
   modalWindow.classList.toggle("popup_is-opened");
 }
-
 function openPopup() {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
   toggleModalWindow();
 }
-
-openButton.addEventListener("click", openPopup);
-
-closeButton.addEventListener("click", toggleModalWindow);
-
 function editProfile(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
   toggleModalWindow();
 }
-
+popupOpenButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", toggleModalWindow);
 editForm.addEventListener("submit", editProfile);
