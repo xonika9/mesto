ffbd7fd8c3d43633cd9b5db72b03c69e30553d88
Alex Beltyukov <xonika9@gmail.com>
2022-04-01 17:37:37 +0800

    кавычки mode: pro

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-const popupOpenButton = document.querySelector(".profile__edit-button");
-const profileName = document.querySelector(".profile__name");
-const profileAbout = document.querySelector(".profile__about");
-const modalWindow = document.querySelector(".popup");
-const editForm = modalWindow.querySelector(".popup__form");
-const nameInput = modalWindow.querySelector(".popup__input_type_name");
-const aboutInput = modalWindow.querySelector(".popup__input_type_about");
-const submitButton = modalWindow.querySelector(".popup__submit-button");
-const popupCloseButton = modalWindow.querySelector(".popup__close-button");
+const popupOpenButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const modalWindow = document.querySelector('.popup');
+const editForm = modalWindow.querySelector('.popup__form');
+const nameInput = modalWindow.querySelector('.popup__input_type_name');
+const aboutInput = modalWindow.querySelector('.popup__input_type_about');
+const submitButton = modalWindow.querySelector('.popup__submit-button');
+const popupCloseButton = modalWindow.querySelector('.popup__close-button');
 function toggleModalWindow() {
-  modalWindow.classList.toggle("popup_is-opened");
+  modalWindow.classList.toggle('popup_is-opened');
 }
 function openPopup() {
   nameInput.value = profileName.textContent;
@@ -21,6 +21,6 @@ function editProfile(evt) {
   profileAbout.textContent = aboutInput.value;
   toggleModalWindow();
 }
-popupOpenButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", toggleModalWindow);
-editForm.addEventListener("submit", editProfile);
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', toggleModalWindow);
+editForm.addEventListener('submit', editProfile);
