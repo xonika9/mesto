fa31e54af5d37c4312775170a902ba80ace1e737
Alex Beltyukov <xonika9@gmail.com>
2022-04-12 20:39:12 +0800

    create cards.js, rename class, fix text in popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,8 @@
-const initialCards = [
-  {
-    name: 'Каньон Антилопы',
-    link: 'https://images.unsplash.com/photo-1648405680499-ab4153bc7be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    name: 'Фаро',
-    link: 'https://images.unsplash.com/photo-1647897478739-7e5e6a31c7f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
-  },
-  {
-    name: 'Большой каньон',
-    link: 'https://images.unsplash.com/photo-1648405680438-48bee6bfee72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    name: 'Парати',
-    link: 'https://images.unsplash.com/photo-1647971450227-650677ca99cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    name: 'Сан-Франциско',
-    link: 'https://images.unsplash.com/photo-1647748918847-b72cf7d2908c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    name: 'Ферма Wooden Shoe',
-    link: 'https://images.unsplash.com/photo-1554261197-de743c7e0ad3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-];
-const imagePopupContainer = document.querySelector('.popup_type-image');
+const imagePopupContainer = document.querySelector('.popup_type_image');
 const imageCloseButton = imagePopupContainer.querySelector('.popup__close-button');
 const popupImage = imagePopupContainer.querySelector('.popup__image');
 const popupCaption = imagePopupContainer.querySelector('.popup__caption');
-const profilePopupContainer = document.querySelector('.popup_type-edit-profile');
+const profilePopupContainer = document.querySelector('.popup_type_edit-profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
@@ -36,7 +10,7 @@ const nameInput = profilePopupContainer.querySelector('.popup__input_type_name')
 const aboutInput = profilePopupContainer.querySelector('.popup__input_type_about');
 const profileCloseButton = profilePopupContainer.querySelector('.popup__close-button');
 const profileEditForm = profilePopupContainer.querySelector('.popup__form');
-const cardPopupContainer = document.querySelector('.popup_type-add-card');
+const cardPopupContainer = document.querySelector('.popup_type_add-card');
 const cardAddButton = document.querySelector('.profile__add-button');
 const titleInput = cardPopupContainer.querySelector('.popup__input_type_card-title');
 const linkInput = cardPopupContainer.querySelector('.popup__input_type_image-link');
@@ -47,7 +21,6 @@ const template = document.querySelector('.card__template');
 function getCardsList() {
   const cardsList = initialCards.map(getCard);
   cardsContainer.append(...cardsList);
-  return cardsContainer;
 }
 function getCard(item) {
   const newCard = template.content.cloneNode(true);
