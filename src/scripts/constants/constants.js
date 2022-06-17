ad5abaa0a46900dbf780236e8ed0c9fa67734eee
Alex Beltyukov <xonika9@gmail.com>
2022-06-17 14:38:59 +0800

    add new selectors

--- src/scripts/constants/constants.js
+++ src/scripts/constants/constants.js
@@ -1,22 +1,30 @@
+const imagePopupSelector = '.popup_type_image';
+const profilePopupSelector = '.popup_type_edit-profile';
+const cardPopupSelector = '.popup_type_add-card';
+const profileName = '.profile__name';
+const profileAbout = '.profile__about';
+
 const imagePopupContainer = document.querySelector('.popup_type_image');
+const profilePopupContainer = document.querySelector('.popup_type_edit-profile');
+const cardPopupContainer = document.querySelector('.popup_type_add-card');
+
 const popupImage = imagePopupContainer.querySelector('.popup__image');
 const popupCaption = imagePopupContainer.querySelector('.popup__caption');
-const profilePopupContainer = document.querySelector('.popup_type_edit-profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profileName = document.querySelector('.profile__name');
-const profileAbout = document.querySelector('.profile__about');
 const nameInput = profilePopupContainer.querySelector('.popup__input_type_name');
 const aboutInput = profilePopupContainer.querySelector('.popup__input_type_about');
 const profileEditForm = profilePopupContainer.querySelector('.popup__form');
-const cardPopupContainer = document.querySelector('.popup_type_add-card');
 const cardAddButton = document.querySelector('.profile__add-button');
 const titleInput = cardPopupContainer.querySelector('.popup__input_type_card-title');
 const linkInput = cardPopupContainer.querySelector('.popup__input_type_image-link');
 const cardAddForm = cardPopupContainer.querySelector('.popup__form');
-const cardsContainer = document.querySelector('.elements');
+const cardsContainer = '.elements';
 const cardTemplate = document.querySelector('.card__template');
 const formValidators = {};
 export {
+  imagePopupSelector,
+  profilePopupSelector,
+  cardPopupSelector,
   imagePopupContainer,
   popupImage,
   popupCaption,
