b4a51a841ef65dd4282a57e19a0031548a6e9e84
Alex Beltyukov <xonika9@gmail.com>
2022-06-19 13:53:26 +0800

    fix

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -1,5 +1,6 @@
 class Card {
   constructor({ title, link }, templateSelector, handleCardClick) {
+    console.log({ title, link });
     (this._title = title),
       (this._link = link),
       (this._templateSelector = templateSelector),
@@ -12,25 +13,29 @@ class Card {
       .cloneNode(true);
     return cardElement;
   };
-  _toggleLike = (evt) => {
+  _toggleLike = () => {
     this._likeButton.classList.toggle('card__like_active');
   };
   _handleRemove = () => {
     this._card.remove();
     this._card = null;
   };
+  _setEventListeners = () => {
+    this._likeButton.addEventListener('click', this._toggleLike);
+    this._removeButton.addEventListener('click', this._handleRemove);
+    this._cardImage.addEventListener('click', () =>
+      this._handleCardClick({ title: this._title, link: this._link })
+    );
+  };
   generateCard = () => {
     this._card = this._getTemplate();
     this._cardImage = this._card.querySelector('.card__photo');
     this._likeButton = this._card.querySelector('.card__like');
+    this._removeButton = this._card.querySelector('.card__remove');
     this._card.querySelector('.card__title').textContent = this._title;
     this._cardImage.src = this._link;
     this._cardImage.alt = this._title;
-    this._likeButton.addEventListener('click', this._toggleLike);
-    this._card.querySelector('.card__remove').addEventListener('click', this._handleRemove);
-    this._cardImage.addEventListener('click', () =>
-      this._handleCardClick({ title: this._title, link: this._link })
-    );
+    this._setEventListeners();
     return this._card;
   };
 }
