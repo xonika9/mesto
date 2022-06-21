876c95c55437adceca554265b0c6360cae2578fb
Alex Beltyukov <xonika9@gmail.com>
2022-06-21 18:30:28 +0800

    add new constants

--- src/scripts/constants/constants.js
+++ src/scripts/constants/constants.js
@@ -3,10 +3,13 @@ const profileEditPopupSelector = '.popup_type_edit-profile';
 const cardAddPopupSelector = '.popup_type_add-card';
 const profileNameSelector = '.profile__name';
 const profileAboutSelector = '.profile__about';
+const profileAvatarSelector = '.profile__avatar';
 const cardsContainerSelector = '.elements';
 const cardTemplateSelector = '.card__template';
 const profileEditButton = document.querySelector('.profile__edit-button');
 const cardAddButton = document.querySelector('.profile__add-button');
+const confirmPopupSelector = '.popup_type_confirm';
+const avatarPopupSelector = '.popup_type_update-avatar';
 
 export {
   imagePopupSelector,
@@ -14,8 +17,11 @@ export {
   cardAddPopupSelector,
   profileNameSelector,
   profileAboutSelector,
+  profileAvatarSelector,
   cardsContainerSelector,
   cardTemplateSelector,
   profileEditButton,
   cardAddButton,
+  confirmPopupSelector,
+  avatarPopupSelector,
 };
